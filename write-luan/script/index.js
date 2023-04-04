171937633f5b7bc5684198b63c449c664faf1d00
luanhsr <107119057+luanhsr@users.noreply.github.com>
2023-04-04 03:29:04 -0300

    0.4.2 salvamento cancelado inserido formulas
    nao consegui criar o salvamento de notas como queria, entao desisti e parti para outras funcoes, deixando ele de maneira funcional, salva uma nota por vez.

--- write-luan/script/index.js
+++ write-luan/script/index.js
@@ -16,15 +16,34 @@ window.addEventListener("load", function () {
     const colorInput = document.querySelector("#inputColor");             
     const selectFont = document.querySelector("#selectFont");
     const fontTall = document.querySelector("#fontTall");
-    const btnTopic = document.querySelector("#btnTopic");
-    const btnTable = document.querySelector("#btnTable");
-    const btnText = document.querySelector("#btnCreateTextBox");
-    const btnFormula = document.querySelector("#btnFormula");
+    const topicBtn = document.querySelector("#btnTopic");
+    const tableBtn = document.querySelector("#btnTable");
+    const textbtn = document.querySelector("#btnCreateTextBox");
+    const formulaBtn = document.querySelector("#btnFormula");
     const imgInput = document.querySelector("#imageInput");
     const audioInput = document.querySelector("#inputAudio");
     const videoInput = document.querySelector("#inputVideo");
-    const btnSave = document.querySelector("#save");
-    const btnDown = document.querySelector("#downPDF");
+    const saveBtn = document.querySelector("#save");
+    const downBtn = document.querySelector("#downPDF");
+
+    function fSave () {
+        let nameNote = prompt('qual nome da nota?');  
+        notes = document.getElementById('notes'); // pegando a lista (nota)
+        let li = document.createElement('li');
+        li.innerHTML = nameNote; // fazendo com que a nota tenha o nome inserido no promppt
+        li.setAttribute('id' ,  n+ 'selectNote' ); // inserindo o atributo para posteriormente selecionar a nota desejada
+        n++
+        localStorage.n = n
+        notes.appendChild(li) // inserindo li na lista de mptas
+        localStorage.info = document.getElementById('papermain').innerHTML; // salvando a informacao que o usuario digitou no cache .info
+        localStorage.liNotes = document.getElementById('notes').innerHTML; // inserindo as notas laterais salvas tambem no local storage
+        arrNotes.push(localStorage.info); // insere a nota salva no cache tambem dentro de um array, onde estarao todas as notas, pois o localstorage nao salva arrays apenas string
+        arrNotes.push('§--byLuanHenrique--§'); // isso e uma separa cada nota de um array, durante a conversao sera excluido
+
+        var saveArrNotes = arrNotes; // um array recebendo todas as notas salvas
+        transfornNotesString = saveArrNotes.toString();  // transformando todas as notas salvas ou seja todo o array em uma string, para que possa ser salvo no localStorage, visto que so salva string
+        localStorage.allNotes = transfornNotesString; // agora sim salvo no cache todas as notas salvas separadas por §--byLuanHenrique--§
+    }
 
     boldBtn.addEventListener('click' , ()=> {                                
         document.execCommand('bold');
@@ -51,7 +70,7 @@ window.addEventListener("load", function () {
         comparei os valores em um outro editor de texto e deixei aproximadamente no HTML.      limitacao do execcomand
         */
     });
-    btnTopic.addEventListener('click' , ()=> {
+    topicBtn.addEventListener('click' , ()=> {
         document.execCommand( "insertUnorderedList"); // cria uma lista não ordenada, por algum motivo o css padding=0   deixa esse evento impossibilitado !!!POR ISSO NAO DEVE SER INSERIDO NO css!!!
     });
     // depois atualizar essa tabela para que ela possa ser movida
@@ -60,7 +79,7 @@ window.addEventListener("load", function () {
     exitT.innerText = "...";  
      // para poder sair dos elementos criados (gambearra)
 
-    btnTable.addEventListener('click' , () => { 
+    tableBtn.addEventListener('click' , () => { 
         let linha = +prompt('Qual número de linhas?'); // Cria mensagem no pronpt (tipo um alert mas com um imput para inserir dados) do usuario perguntando quantas linhas serao inseridas, salvando na variavel linha 
         let coluna= +prompt('Qual número de colunas?'); // - obs importante o + na frente serve para transformar o que for inserido em int em vez de string 
         // substituir por um MODAL **
@@ -92,24 +111,41 @@ window.addEventListener("load", function () {
             papermain.appendChild(exitT); // o pontinho pra fora apenas para poder clicar para sair
         }
     });
-    btnText.addEventListener('click' , ()=> {
+    textbtn.addEventListener('click' , ()=> {
         let t = document.createElement('div');
-        t.style.height ="150px";
-        t.style.width ="150px";
-        t.innerHTML = "digite seu texto aqui!"
-        t.style.border = "1px solid black"
+        t.setAttribute("class" , "Quadrado");
         papermain.appendChild(t);
         papermain.appendChild(exitT);
     });
-    btnFormula.addEventListener('click' , ()=> {
-        let t = document.createElement('div');
-        t.style.height ="200px";
-        t.style.width ="200px";
-        t.innerHTML = "FORMULA QUADRADA"
-        t.style.border = "1px solid black"
-        papermain.appendChild(t);
-        papermain.appendChild(exitT);
-        // DEPOIS fazer mais formulas e alterar para um select no HTML*
+    formulaBtn.addEventListener ('click', ()=> {
+        switch (formulaBtn.value) {
+            case 'quadrado':
+                let q = document.createElement('div');
+                q.setAttribute("class" , "quadrado");
+                papermain.appendChild(q);
+                papermain.appendChild(exitT);
+                formulaBtn.value = "null"
+            break;
+            case 'circulo':
+                let c = document.createElement('div');
+                c.setAttribute("class" , "circulo");
+                papermain.appendChild(c);
+                papermain.appendChild(exitT);
+                formulaBtn.value = "null"
+            break;
+            case 'estrela':
+                console.log('estrela') 
+                formulaBtn.value = "null"
+            break;
+            case 'triangulo':
+                let t = document.createElement('div');
+                t.setAttribute("class" , "triangulo");
+                papermain.appendChild(t);
+                papermain.appendChild(exitT);
+                formulaBtn.value = "null"
+            break;         
+        } 
+        return true;
     });
     imgInput.addEventListener('change', function (){
         if (this.files && this.files[0]) {
@@ -154,112 +190,18 @@ window.addEventListener("load", function () {
 
      } 
    }); 
-
-   if (localStorage.allNotes != null) { // quando a pagina recarregar vai verificar se tem algum elemento em todas as notas salvas
-        arrNotesTransformToArray = localStorage.allNotes; // feito a verificacao cria-se um array para receber todas as notas salvas em formato de string separado uma a uma por §--byLuanHenrique--§
-            
-        result = arrNotesTransformToArray.search(`-byLuanHenrique-`); // isso verifica se existe a separacao, se nao ira excluir notas validas, a funcao search procura elementos citados e retorna o valor da sua posicao, se nao tiver ele retorna -1 se tiver vai mostrar na onde esta na string
-        if (result >= 0) { // ou seja, se tiver qualquer §--byLuanHenrique--§  em qualquer lugar sera feito a logica
-            arrNotes = arrNotesTransformToArray.split(',§--byLuanHenrique--§,'); // basicamente o array volta a receber todas as notas salvas, separando em cada indice cada nota, sendo separadas por §--byLuanHenrique--§
-        }
-        
-    } 
+    
     if (localStorage.info != null) {
             document.querySelector("#papermain").innerHTML =  localStorage.info;
-            n = localStorage.n
-            parseInt(n);
-            document.querySelector("#notes").innerHTML = localStorage.liNotes;
-            var enlistment =  document.querySelectorAll("#notes li"); // selecionando todos os elementos dentro das #notas que sejam li, ou seja a lista de notas
-            for (var i = 0; i <enlistment.length; i ++) { // percorrendo o alistamento 
-                enlistment[i].addEventListener("click" , function(e){ // ao selecionar um indice deste alistamento ira adicionar o evento clicar e ira mostrar o id selecionado.
-                let select = this.id
-                document.querySelector('#papermain').innerHTML = arrNotes[parseInt(select)]
-                })
-            }
-            
-
-    } else {
-        document.querySelector("#notes").innerHTML = "Nem uma nota salva: "
-        n = 0;
     }
-    // a logica a baixo e para garantir que toda vez que o usuario clicar em salvar, ira salvar uma nota em cada indice de array e criar uma separacao para que posteriormente possa ser 
-    // separado novamente, se nao ao recarregar a pagina todos os indices (notas) estarao em uma unica nota e em um unico indice
 
-    btnSave.addEventListener('click' , () =>{
-
-            if (arrNotes == 0) { // 1 caso a array ainda estiver vazio salva normalmente
-                let nameNote = prompt('qual nome da nota?');  
-                notes = document.getElementById('notes'); // pegando a lista (nota)
-                let li = document.createElement('li');
-                li.innerHTML = nameNote; // fazendo com que a nota tenha o nome inserido no promppt
-                li.setAttribute('id' ,  n+ 'selectNote' ); // inserindo o atributo para posteriormente selecionar a nota desejada
-                n++
-                localStorage.n = n
-                notes.appendChild(li) // inserindo li na lista de mptas
-                localStorage.info = document.getElementById('papermain').innerHTML; // salvando a informacao que o usuario digitou no cache .info
-                localStorage.liNotes = document.getElementById('notes').innerHTML; // inserindo as notas laterais salvas tambem no local storage
-                arrNotes.push(localStorage.info); // insere a nota salva no cache tambem dentro de um array, onde estarao todas as notas, pois o localstorage nao salva arrays apenas string
-                arrNotes.push('§--byLuanHenrique--§'); // isso e uma separa cada nota de um array, durante a conversao sera excluido
-
-                var saveArrNotes = arrNotes; // um array recebendo todas as notas salvas
-                transfornNotesString = saveArrNotes.toString();  // transformando todas as notas salvas ou seja todo o array em uma string, para que possa ser salvo no localStorage, visto que so salva string
-                localStorage.allNotes = transfornNotesString; // agora sim salvo no cache todas as notas salvas separadas por §--byLuanHenrique--§
-                
-            } 
-            else if (arrNotes[arrNotes.length-1] == '§--byLuanHenrique--§' && arrNotes.length > 0) { // quando a pagina recarregar e tiver esse array com a separacao continua salvando normalmente, se nao ira ser inserido os elementos do array com a separacao
-                let nameNote = prompt('qual nome da nota?'); 
-                notes = document.getElementById('notes'); // pegando a lista (nota)
-                let li = document.createElement('li');
-                li.innerHTML = nameNote; // fazendo com que a nota tenha o nome inserido no promppt
-                li.setAttribute('id' ,  n+ 'selectNote');
-                n++
-                localStorage.n = n
-                notes.appendChild(li) // pinserindo li na lista
-                localStorage.info = document.getElementById('papermain').innerHTML;
-                localStorage.liNotes = document.getElementById('notes').innerHTML; // inserindo as notas laterais salvas tambem no local storage
-                arrNotes.push(localStorage.info); // insere o que esta no localstorage dentro do array
-                arrNotes.push('§--byLuanHenrique--§'); // isso e uma separacao dos arrays
-
-                var saveArrNotes = arrNotes;
-                transfornNotesString = saveArrNotes.toString();
-                localStorage.allNotes = transfornNotesString;
-
-            } 
-            else { // quando o array identificar que ja foi feito a separacao, ele deve inserir a separacao novamente a cada indice do array
-                    
-
-                let element = '§--byLuanHenrique--§';  // cria o elemento que separa os indices 
-                let insertInArray = []; // cria um array vazio para receber tudo
-                arrNotes.forEach((e, i) => { //percorre o array buscando os elementos e os indices
-                    if (i === 0) { // se o indice for indentico a 0 ou seja o primeiro indice
-                            return insertInArray = [e, element] // ira retornar o elemento do array com o novo elemento
-                    }
-                    insertInArray.push(e, element) // depois de feito pela primeira vez, ira a cada elementod do array inserir o novo elemento de separacao
-                    })
-                    // pronto, agora toda vez que a pagina recarregar e as paginas nao estiverem separadas, ira ser realizado a separacao 
-                arrNotes= insertInArray;
-
-                let nameNote = prompt('qual nome da nota?'); 
-                notes = document.getElementById('notes'); // pegando a lista (nota)
-                let li = document.createElement('li');
-                li.innerHTML = nameNote; // fazendo com que a nota tenha o nome inserido no promppt
-                li.setAttribute('id' ,  n+ 'selectNote');
-                n++
-                localStorage.n = n
-                notes.appendChild(li) // pinserindo li na lista
-                localStorage.info = document.getElementById('papermain').innerHTML;
-                localStorage.liNotes = document.getElementById('notes').innerHTML; // inserindo as notas laterais salvas tambem no local storage
-                arrNotes.push(localStorage.info); // insere o que esta no localstorage dentro do array
-                arrNotes.push('§--byLuanHenrique--§'); // isso e uma separacao dos arrays
+    saveBtn.addEventListener('click' , () =>{
+ 
+        localStorage.info = document.getElementById('papermain').innerHTML;
     
-                var saveArrNotes = arrNotes;
-                transfornNotesString = saveArrNotes.toString();
-                localStorage.allNotes = transfornNotesString;
-
-            }    
     });
    
-   btnDown.addEventListener('click' , () => {
+   downBtn.addEventListener('click' , () => {
         var doc = new jsPDF();
         doc.fromHTML(document.getElementById("papermain"), // page element which you want to print as PDF
         15,
